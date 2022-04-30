1f09078af5725fa20f34e162abc29d3fe7035b57
Eunbi <eunbkim4@gmail.com>
2022-04-30 19:32:04 +0900

    게시글 등록/수정/조회 화면 구성

--- springboot-webservice/src/main/java/com/study/springbootwebservice/service/posts/PostsService.java
+++ springboot-webservice/src/main/java/com/study/springbootwebservice/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.study.springbootwebservice.service.posts;
 
 import com.study.springbootwebservice.domain.posts.Posts;
 import com.study.springbootwebservice.domain.posts.PostsRepository;
+import com.study.springbootwebservice.web.dto.PostsListResponseDto;
 import com.study.springbootwebservice.web.dto.PostsResponseDto;
 import com.study.springbootwebservice.web.dto.PostsSaveRequestDto;
 import com.study.springbootwebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,10 +34,18 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
---
+++ springboot-webservice/src/test/java/com/study/springbootwebservice/web/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.study.springbootwebservice.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void loadMainPage() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹서비스");
+    }
+}
