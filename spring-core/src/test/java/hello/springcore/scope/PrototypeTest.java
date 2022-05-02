22aeb9ac67466d0c092c52cc1d1df341232c89b2
Eunbi <eunbkim4@gmail.com>
2022-05-03 05:55:05 +0900

    빈 스코프

---
+++ spring-core/src/test/java/hello/springcore/scope/PrototypeTest.java
@@ -0,0 +1,41 @@
+package hello.springcore.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1"); // 빈을 조회할 때마다 새로운 객체 생성
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+//      ac.close();
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy // 호출 안됨
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
