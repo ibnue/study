422c07311cc142b1af27f37034861a40272f96d2
Eunbi <eunbkim4@gmail.com>
2022-05-04 04:13:15 +0900

    프로젝트 환경설정

---
+++ jpashop/src/test/java/jpa/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package jpa.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false) // 롤백 안하고 커밋
+    void testMember() {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); // 영속성 컨텍스트에서 식별자가 같으면 같은 엔티티로 인식
+    }
+}
