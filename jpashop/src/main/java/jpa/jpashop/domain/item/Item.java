5998ccb014d7b7b92b059137df7898950a9d005d
Eunbi <eunbkim4@gmail.com>
2022-05-07 05:05:46 +0900

    상품 기능 추가

--- jpashop/src/main/java/jpa/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpa/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpa.jpashop.domain.item;
 
 import jpa.jpashop.domain.Category;
+import jpa.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id
@@ -27,4 +28,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비지니스 로직
+    // 데이터를 가지고 있는 쪽에 비지니스 메서드가 있는 것이 좋다, 응집력 높은 객체지향적 설계.
+    /**
+     *  stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *  stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ jpashop/src/main/java/jpa/jpashop/service/ItemService.java
@@ -0,0 +1,31 @@
+package jpa.jpashop.service;
+
+import jpa.jpashop.domain.item.Item;
+import jpa.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    // 상품 서비스는 리포지토리에 단순하게 위임만 하는 클래스.
+   @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
